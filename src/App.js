ce7663ec86735097baa89fe4faf1892f45159cd5
eyemabhishek <eyemabhishek@gmail.com>
2024-02-01 12:03:02 +0530

    jen

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React - Aws 
+          React - Aws -jenkins
         </p>
         <a
           className="App-link"
