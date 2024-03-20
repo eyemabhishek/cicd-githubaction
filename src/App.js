ab6307942b9a85d80b3bebdf2e94b2c7f06cf0ec
Abhishek Kumar <81435274+kumarabhishek-github@users.noreply.github.com>
2024-03-20 23:41:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React - Aws -Github
+          React - Aws -Github-success
         </p>
         <a
           className="App-link"
